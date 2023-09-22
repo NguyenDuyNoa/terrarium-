02aad8aa28a1a00340810570b4f8c1897fb49a6d
Duy Noa Nguyễn <duynoanguyen@Duys-MacBook-Pro.local>
2023-09-22 13:01:05 +0700

    third commit

--- js/product-list.js
+++ js/product-list.js
@@ -81,11 +81,35 @@ for (var i = 0; i < products.length; i++) {
     
     var productCard = document.createElement("div");
     productCard.classList.add("product-card");
+
+    var imageProduct = document.createElement("div");
+    imageProduct.classList.add("image-product");
     
     var productImage = document.createElement("img");
     productImage.src = product.imageUrl;
     productImage.alt = product.name;
+
+    var actions = document.createElement("div");
+    actions.classList.add("actions");
+
+    var favoriteButton = document.createElement("button");
+    favoriteButton.classList.add("favorite");
+
+    var iconHeart = document.createElement("i");
+    iconHeart.classList.add("fa-regular", "fa-heart");
     
+    var cartButton = document.createElement("button");
+    cartButton.classList.add("cart");
+    
+    var iconCart = document.createElement("i");
+    iconCart.classList.add("fa-solid", "fa-cart-shopping");
+    
+    favoriteButton.appendChild(iconHeart);
+    cartButton.appendChild(iconCart);
+
+    actions.appendChild(favoriteButton);
+    actions.appendChild(cartButton);
+
     var productTitle = document.createElement("h3");
     productTitle.classList.add("card-title");
 
@@ -97,7 +121,10 @@ for (var i = 0; i < products.length; i++) {
     var productPrice = document.createElement("h4");
     productPrice.textContent = product.price;
     
-    productCard.appendChild(productImage);
+    imageProduct.appendChild(productImage);
+    imageProduct.appendChild(actions);
+
+    productCard.appendChild(imageProduct);
     productCard.appendChild(productTitle);
     productCard.appendChild(productPrice);
     
